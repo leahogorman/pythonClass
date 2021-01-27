af1f8e139e447ba305ffd3e97b2df186503a91b6
Leah O'Gorman <leah.p.ogorman@gmail.com>
2021-01-27 18:23:48 -0500

    Initial commit

---
+++ Class1/hello_world.py
@@ -0,0 +1,17 @@
+my_name = "Leah"
+a_number = 42
+
+print("My name is" + my_name)
+print(2+3)
+print(1.01 + 1.02)
+
+print("My name is " + my_name + " and the number is " + str(a_number))
+
+num_1 = input("Enter the first Integer >>> ")
+num_2 = input("Enter the second Integer >>> ")
+
+sum_of_numbers = int(num_1) + int(num_2)
+
+print("The sum of the two numbers is " + sum_of_numbers)
+
+
